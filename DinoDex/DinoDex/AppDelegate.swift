1647248be3b4d28a78c16262ddb2a35feedcd6d7
Nathan Hekman <hekman.nathan@gmail.com>
2016-03-14 18:47:13 -0500

    Added background music to main screen

--- DinoDex/DinoDex/AppDelegate.swift
+++ DinoDex/DinoDex/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import AudioToolbox
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var player : AVAudioPlayer! = nil // will be Optional, must supply initializer
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        startBackgroundMusic()
         return true
     }
 
@@ -27,10 +31,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        player.pause()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        player.play()
+        
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -39,6 +46,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    
+    func startBackgroundMusic() {
+        if let path = NSBundle.mainBundle().pathForResource("background", ofType:"mp3") {
+            let fileURL = NSURL(fileURLWithPath: path)
+            do {
+                player = try AVAudioPlayer(contentsOfURL: fileURL)
+                player.numberOfLoops = -1
+                player.volume = 0.5
+                player.prepareToPlay()
+                //player.delegate = self
+                player.play()
+            }
+            catch {
+                print("Error getting the audio file")
+            }
+        }
     }
 
 
--- DinoDex/DinoDex/Controllers/CustomFocusEffectsCollectionViewController.swift
+++ DinoDex/DinoDex/Controllers/CustomFocusEffectsCollectionViewController.swift
@@ -1,36 +1,14 @@
-//
-//  CustomFocusEffectsCollectionViewController.swift
-//  tvOS-Examples
-//
-//  Copyright (c) 2016 WillowTree, Inc.
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
-//
-//
+
 
 import UIKit
 import SceneKit
 import StoreKit
 
+
 private let reuseIdentifier = "Cell"
 
 class CustomFocusEffectsCollectionViewController: UICollectionViewController {
+    
     
     // This list of available in-app purchases
     var products = [SKProduct]()
@@ -54,7 +32,10 @@ class CustomFocusEffectsCollectionViewController: UICollectionViewController {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "productPurchased:", name: IAPHelperProductPurchasedNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "productPurchaseFailed:", name: IAPHelperProductFailedNotification, object: nil)
         
+        
     }
+    
+
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
